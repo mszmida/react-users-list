c9f4d0b7d5cf451df9df36fe8d4c04cd9b5968c3
Maciej Szmida <maciek.bmg@gmail.com>
2020-01-23 01:27:21 +0100

    Added <App /> root component smoke test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App /> root component', () => {
+  test('renders without errors', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/^users list/i);
+
+    expect(linkElement).toBeInTheDocument();
+  });
 });
